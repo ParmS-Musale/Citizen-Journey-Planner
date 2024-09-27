f310ddac8831c3abab314b799acb0dc01b996dc8
ParmS-Musale <musaleparm9541@gmail.com>
2024-09-27 18:06:21 +0530

    Host On Onrender..!!

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db.js');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
-connectDB();
+
 
 const app = express();
 app.use(cors());
@@ -15,6 +15,11 @@ app.use('/api/auth', authRoutes); // Use authentication routes
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+connectDB().then(()=>{
+  console.log("database Connection Successful");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(err => console.log("error occured in db ",err)
+)
